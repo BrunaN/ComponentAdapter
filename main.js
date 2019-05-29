d2690ed1274b49396e6068df52de56a7a9659d60
Bruna Neves <brubsnev@gmail.com>
2019-05-29 20:55:05 -0300

    Componente de texto, alteração de tamanho

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 let text = document.getElementById('text');
+let increaseButton = document.getElementById('increase');
+let decreaseButton = document.getElementById('decrease');
+
+increaseButton.addEventListener("click", function(){
+    text.style.fontSize = '20px'
+});
