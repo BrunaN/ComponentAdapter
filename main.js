eebbb2fb6c13bcbc9759289702a4fef77667d643
Bruna Neves <brubsnev@gmail.com>
2019-05-29 23:11:24 -0300

    textos

--- main.js
+++ main.js
@@ -1,30 +1,64 @@
 let text = document.getElementById('text');
 let increaseButton = document.getElementById('increase');
 let decreaseButton = document.getElementById('decrease');
+let increaseTextLineButton = document.getElementById('increase-text-line');
+let decreaseTextLineButton = document.getElementById('decrease-text-line');
 
 let content = document.getElementsByClassName('content');
 
-let style = window.getComputedStyle(text, null);
+let paragraph = document.getElementsByTagName('p');
 
 let next = document.getElementById("next");
 let countContent = 0;
 
+let style = window.getComputedStyle(text, null);
+let fontSize = style.getPropertyValue('font-size');
+
+let styleParagraph = window.getComputedStyle(paragraph[1], null);
+let lineHeight = styleParagraph.getPropertyValue('line-height');
+
+increaseButton.addEventListener("click", function(){
+    let x = parseInt(fontSize.replace("px",""));
+
+    text.style.fontSize = x + 5 +'px';
+    fontSize = style.getPropertyValue('font-size');
+});
+
+decreaseButton.addEventListener("click", function(){
+    let x = parseInt(fontSize.replace("px",""));
+
+    text.style.fontSize = x - 5 +'px';
+    fontSize = style.getPropertyValue('font-size');
+});
+
 next.addEventListener("click", function(){
+    
+    for(let i = 0; i < paragraph.length; i++){
+        paragraph[i].style.fontSize = fontSize;
+    } 
+
     content[countContent].style.display = 'none';
     countContent++;
     content[countContent].style.display = 'flex';
 });
 
-increaseButton.addEventListener("click", function(){
-    let fontSize = style.getPropertyValue('font-size');
-    let x = parseInt(fontSize.replace("px",""));
 
-    text.style.fontSize = x + 5 +'px'
+increaseTextLineButton.addEventListener("click", function(){
+    let x = parseInt(lineHeight.replace("px",""));
+    
+    for(let i = 0; i < paragraph.length; i++){
+        paragraph[i].style.lineHeight = x + 2 + 'px';
+    } 
+
+    lineHeight = styleParagraph.getPropertyValue('line-height');
 });
 
-decreaseButton.addEventListener("click", function(){
-    let fontSize = style.getPropertyValue('font-size');
-    let x = parseInt(fontSize.replace("px",""));
+decreaseTextLineButton.addEventListener("click", function(){
+    let x = parseInt(lineHeight.replace("px",""));
+    
+    for(let i = 0; i < paragraph.length; i++){
+        paragraph[i].style.lineHeight = x - 2 + 'px';
+    } 
 
-    text.style.fontSize = x - 5 +'px'
+    lineHeight = styleParagraph.getPropertyValue('line-height');
 });
