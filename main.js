1efaa24c2ac26f0a766cab20d0db31a4026223ef
Bruna Neves <brubsnev@gmail.com>
2019-05-29 21:36:15 -0300

    Arquivos do protótipo

--- main.js
+++ main.js
@@ -2,6 +2,24 @@ let text = document.getElementById('text');
 let increaseButton = document.getElementById('increase');
 let decreaseButton = document.getElementById('decrease');
 
+let style = window.getComputedStyle(text, null);
+
+let next = document.getElementById("next");
+
+next.addEventListener("click", function(){
+    
+});
+
 increaseButton.addEventListener("click", function(){
-    text.style.fontSize = '20px'
+    let fontSize = style.getPropertyValue('font-size');
+    let x = parseInt(fontSize.replace("px",""));
+
+    text.style.fontSize = x + 5 +'px'
+});
+
+decreaseButton.addEventListener("click", function(){
+    let fontSize = style.getPropertyValue('font-size');
+    let x = parseInt(fontSize.replace("px",""));
+
+    text.style.fontSize = x - 5 +'px'
 });
